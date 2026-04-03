ebe09a84cf5e5343ebc027001af7931c42921a1e
pvjambur <145439975+pvjambur@users.noreply.github.com>
2026-04-03 21:34:59 +0530

    Add: palindrome-number - submission-1

---
+++ Data Structures & Algorithms/palindrome-number/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0) return false;
+        if (x<10) return true;
+        int n = x;
+        vector<int> nums;
+        while (n!=0){
+            nums.push_back(n%10);
+            n = n/10;
+        }
+        int r = nums.size()-1;
+        int l = 0;
+        while (l<r){
+            if (nums[l]!=nums[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
