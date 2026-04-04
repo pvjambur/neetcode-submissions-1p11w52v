750c4bc9022800b0d92d837beadc0e68830d9dfd
pvjambur <145439975+pvjambur@users.noreply.github.com>
2026-04-05 00:14:18 +0530

    Add: spiral-matrix-ii - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix-ii/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> grid(n,vector<int>(n,0));
+        int l = 0, r = n-1, t = 0, b = n-1;
+        int num = 1;
+        while (l<=r || t<=b){
+            for (int i=l;i<=r;i++){
+                grid[t][i] = num++;
+            }
+            t++;
+            for (int i=t;i<=b;i++){
+                grid[i][r] = num++;
+            }
+            r--;
+            for (int i=r;i>=l;i--){
+                grid[b][i] = num++;
+            }
+            b--;
+            for (int i=b;i>=t;i--){
+                grid[i][l] = num++;
+            }
+            l++;
+        }
+        if (n%2==1) grid[n/2][n/2] = n*n;
+        return grid;
+    }
+};
