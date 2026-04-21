d0c702af018f0b0341568baf25875a5ee781be61
pvjambur <145439975+pvjambur@users.noreply.github.com>
2026-04-21 22:53:37 +0530

    Add: design-circular-queue - submission-0

---
+++ Data Structures & Algorithms/design-circular-queue/submission-0.cpp
@@ -0,0 +1,49 @@
+class MyCircularQueue {
+private:
+    vector<int> q;
+    int front, rear, size, capacity;
+
+public:
+    MyCircularQueue(int k) {
+        capacity = k;
+        q.resize(k);
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+    
+    bool enQueue(int value) {
+        if (isFull()) return false;
+        
+        rear = (rear + 1) % capacity;
+        q[rear] = value;
+        size++;
+        return true;
+    }
+    
+    bool deQueue() {
+        if (isEmpty()) return false;
+        
+        front = (front + 1) % capacity;
+        size--;
+        return true;
+    }
+    
+    int Front() {
+        if (isEmpty()) return -1;
+        return q[front];
+    }
+    
+    int Rear() {
+        if (isEmpty()) return -1;
+        return q[rear];
+    }
+    
+    bool isEmpty() {
+        return size == 0;
+    }
+    
+    bool isFull() {
+        return size == capacity;
+    }
+};
