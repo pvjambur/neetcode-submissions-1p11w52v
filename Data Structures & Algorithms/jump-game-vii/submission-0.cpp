a2febaad77ae4ab5c395c7d9bbe3811a730f2c2a
pvjambur <145439975+pvjambur@users.noreply.github.com>
2026-04-24 22:45:22 +0530

    Add: jump-game-vii - submission-0

---
+++ Data Structures & Algorithms/jump-game-vii/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+        vector<int> dp(n,0);
+        dp[0] = 1;
+
+        int reach = 0;
+        for (int i=1;i<n;i++){
+            if (i-minJump>=0){
+                reach += dp[i-minJump]; 
+            }
+            if (i-maxJump-1>=0){
+                reach -= dp[i-maxJump-1];
+            }
+            if (s[i]=='0' && reach>0){
+                dp[i] = 1;
+            }
+        }
+        return dp[n-1];
+    }
+};
