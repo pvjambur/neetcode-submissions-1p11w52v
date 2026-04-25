8f0f8e2ae01a2e2e4ff780cf14f690f00a01b2be
pvjambur <145439975+pvjambur@users.noreply.github.com>
2026-04-25 16:20:50 +0530

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.cpp
@@ -0,0 +1,28 @@
+class MyStack {
+public:
+    queue<int> q;
+
+    void push(int x) {
+        q.push(x);
+        int sz = q.size();
+
+        for (int i = 0; i < sz - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
