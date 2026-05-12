a7971d1b8930faf44c736d96fc9b74d132245e68
pvjambur <145439975+pvjambur@users.noreply.github.com>
2026-05-12 15:46:24 +0530

    Add: delete-node-in-a-bst - submission-0

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-0.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+private:
+    TreeNode* findMin(TreeNode* root) {
+        while (root->left) {
+            root = root->left;
+        }
+        return root;
+    }
+
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return NULL;
+
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        }
+        else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        }
+        else {
+            // Case 1: No child
+            if (!root->left && !root->right) {
+                delete root;
+                return NULL;
+            }
+
+            // Case 2: One child
+            if (!root->left) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+
+            if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            // Case 3: Two children
+            TreeNode* succ = findMin(root->right);
+
+            root->val = succ->val;
+
+            root->right = deleteNode(root->right, succ->val);
+        }
+
+        return root;
+    }
+};
