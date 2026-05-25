517662aca34da8d74a7a8be11840cb0fcbbe84d2
pvjambur <145439975+pvjambur@users.noreply.github.com>
2026-05-25 21:49:52 +0530

    Add: counting-words-with-a-given-prefix - submission-0

---
+++ Data Structures & Algorithms/counting-words-with-a-given-prefix/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int prefixCount(String[] words, String pref) {
+
+        int count = 0;
+
+        for (String s : words) {
+
+            if (s.startsWith(pref)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
