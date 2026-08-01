682ef78f8bd9095864594e5ec096d9c1ab165f45
pvjambur <145439975+pvjambur@users.noreply.github.com>
2026-08-01 15:34:46 +0530

    Add: candy - submission-0

---
+++ Data Structures & Algorithms/candy/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> res(n,1);
+        for (int i=1;i<n;i++){
+            if (ratings[i-1]<ratings[i]){
+                res[i] = res[i-1] + 1;
+            }
+        }
+
+        for (int i=n-2;i>=0;i--){
+            if (ratings[i]>ratings[i+1]){
+                res[i] = max(res[i],res[i+1] + 1);
+            }
+        }
+
+        int count = 0;
+        for (int x: res){
+            count += x;
+        }
+
+        return count;
+    }
+};
