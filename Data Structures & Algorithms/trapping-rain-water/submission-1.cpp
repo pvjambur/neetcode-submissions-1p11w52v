b2c8fca09ca890da76e2ece636c631bdccb2e16f
pvjambur <145439975+pvjambur@users.noreply.github.com>
2026-08-15 18:10:08 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        stack<int> st;
+        int n = height.size();
+        int water = 0;
+        for (int i=0;i<n;i++){
+            while(!st.empty() && height[i]>height[st.top()]){
+                int mid = st.top();
+                st.pop();
+                if (st.empty()) break;
+                int left = st.top();
+                int h = min(height[i],height[left]) - height[mid];
+                int width = i - left - 1;
+                water += h*width;
+            }
+            st.push(i);
+        }
+        return water;
+    }
+};
